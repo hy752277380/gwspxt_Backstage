b35bf5afc42156cf44d0b8073db7d637ca659b21
hy752277380 <33386194+hy752277380@users.noreply.github.com>
2018-06-15 08:48:07 +0800

    所有页面

--- src/main/java/com/kcsj/gwglxt/controller/DocumentManage/DocumentManageController.java
+++ src/main/java/com/kcsj/gwglxt/controller/DocumentManage/DocumentManageController.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class DocumentManageController {
     @Autowired
     private DocumentService documentService;

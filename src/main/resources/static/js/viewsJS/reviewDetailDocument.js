b89ed4dda81a4c25a1ca349f7d9aa6ae393a0a58
hy752277380 <33386194+hy752277380@users.noreply.github.com>
2018-06-21 16:09:39 +0800

    管理跳转到查看文档页面

--- src/main/resources/static/js/viewsJS/reviewDetailDocument.js
+++ src/main/resources/static/js/viewsJS/reviewDetailDocument.js
@@ -1,27 +1,28 @@
-$(function() {
-			var data = {
-				user: JSON.parse(sessionStorage.getItem("loginUser")),
-				lhs_edit: JSON.parse(sessionStorage.getItem("lhs_edit")),
-				docData: '', //所有数据
-			}
-			var detailDoc = new Vue({
-					el: "#main",
-					data: data,
-					methods: {
-						getInfo(params) {
-							$.post('/gwspxt/documentBaseInfo/'+lhs_edit, params, function(response) {
-							}, 'json');
-						},
+$(function () {
+    var data = {
+        user: JSON.parse(sessionStorage.getItem("loginUser")),
+        lhs_edit:JSON.parse(sessionStorage.getItem('lhs_edit')),
+        name: 'reviewDetailDocument',
+        docData: '', //所有数据
+        /*ready: false,
+        page: {
+            allRow: 1,
+            totalPage: 1,
+            currentPage: 1,
+            pageSize: 10,
+            hasPreviousPage: false,
+            hasNextPage: false,
+        },*/
+    }
 
-						mounted() {
-							this.getInfo({
-                             documentId:lhs_edit.doc_id,
-
-							});
-						},
-						watch: {},
-						components: {}
-						}
-					});
-			})
+    var reviewDetailDocument = new Vue({
+        el: "#main",
+        data: data,
+        components: {
+            'asideComponent': Layout,
+            //'page-util': pageUtil,
+            //'search-util': searchUtil
+        }
+    });
+})
 

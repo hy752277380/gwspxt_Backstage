423ef4910d564aab8953562f0e6d473227302546
elias06 <32606960+elias06@users.noreply.github.com>
2018-06-21 09:46:25 +0800

    文档管理

--- src/main/resources/static/js/viewsJS/reviewDetailDocument.js
+++ src/main/resources/static/js/viewsJS/reviewDetailDocument.js
@@ -1,4 +1,44 @@
 $(function() {
+    var lhs_edit=JSON.parse(sessionStorage.getItem("lhs_edit"));
+    var documentId=lhs_edit.doc_id;
+    var documentTitle=$("#documentTitle");
+    var documentNo=$("#documentNo");
+    var documentUser=$("#documentUser");
+    var doucmentContent=$("#doucmentContent");
+    var documentRemark=$("#documentRemark");
+var documentType=$("#documentType");
+     $.ajax({
+                url: "http://localhost:8080/gwspxt/documentBaseInfo",
+                type: "get",
+                timeout: 3000,
+                data: {
+                "documentId":documentId,
+                },
+                dataType: "json",
+                success: function (data) {
+                  documentTitle.val(data.document.documentTitle);
+                  documentNo.val(data.document.documentNo);
+                  documentUser.val(data.guser.userName);
+                  doucmentContent.val(data.document.doucmentContent);
+                  documentRemark.val(data.document.documentRemark);
+                  $('#documentType').find('option[value='+data.document.documentType+']').atrr('selected','selected');
+                  $("input[name=documentConfidential][value="+data.document.documentConfidential+"]").attr("checked",true);
+                  $("input[name=documentSpeed][value="+data.document.documentSpeed+"]").attr("checked",true);
+
+                /*for(var i=1;i<data.length;i++)
+                        {
+                         $('#documentType').append("<option value='' >"+data[i].documentType.documenttypeName+"</option>");
+                        }
+*/
+                }
+            });
+
+})
+
+
+
+/*
+$(function() {
 			var data = {
 				user: JSON.parse(sessionStorage.getItem("loginUser")),
 				lhs_edit: JSON.parse(sessionStorage.getItem("lhs_edit")),
@@ -9,7 +49,7 @@ $(function() {
 					data: data,
 					methods: {
 						getInfo(params) {
-						   console.log(data.lhs_edit.doc_id);
+						  console.log(data.lhs_edit.doc_id);
 							$.post('http://localhost:8080/gwspxt/documentBaseInfo', params, function(response) {
                             detailDoc.docData=response.list;
 
@@ -27,4 +67,5 @@ $(function() {
 						}
 					});
 			})
+*/
 

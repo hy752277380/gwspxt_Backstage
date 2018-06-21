0a8f147c8be1c0bc0b7d3111df652547117c95ec
elias06 <32606960+elias06@users.noreply.github.com>
2018-06-21 16:10:04 +0800

    文档管理

--- src/main/resources/static/js/viewsJS/reviewDetailDocument.js
+++ src/main/resources/static/js/viewsJS/reviewDetailDocument.js
@@ -1,47 +1,42 @@
-var detail;
-
-new Vue({
-    el:'#main',
-                     data:{},
-});
-			/*va
-$(function() {
-			var data = {
-				user: JSON.parse(sessionStorage.getItem("loginUser")),
-				//lhs_edit: JSON.parse(sessionStorage.getItem("lhs_edit")),
-				ready:true,
-				docData: '', //所有数据
-			}
-
-			r detailDoc = new Vue({
-					el:"#main" ,
-					data: data ,
-					methods: {
-						getInfo(params) {
-							let documentId = this.$data.lhs_edit.doc_id;
-							$.post('/gwspxt/documentBaseInfo', {documentId:documentId}, function(response) {
-	                            data.docData = response;
-   							}, 'json');
-						},
-					},
-                    components: {
-                         'asideComponent': Layout,
-                    }
-			});*/
-
 /*
+$(function() {
+    var data = {
+        user: JSON.parse(sessionStorage.getItem("loginUser")),
+        lhs_edit: JSON.parse(sessionStorage.getItem("lhs_edit")),
+        ready: true,
+        docData: '', //所有数据
+    }
+
+    var detailDoc = new Vue({
+        el: "#main",
+        data: data,
+        methods: {
+            getInfo(params) {
+                var documentId = this.$data.lhs_edit.doc_id;
+                $.post('/gwspxt/documentBaseInfo', {documentId: documentId}, function (response) {
+                    detailDoc.docData = response;
+                }, 'json');
+            },
+        },
+        components: {
+            'asideComponent': Layout,
+        }
+    });
+})
+*/
+$(function() {
+
     var lhs_edit=JSON.parse(sessionStorage.getItem("lhs_edit"));
     var documentId=lhs_edit.doc_id;
-    var operate=lhs_edit.action;
-
+    /*var operate=lhs_edit.action;*/
     var documentTitle=$("#documentTitle");
     var documentNo=$("#documentNo");
     var documentUser=$("#documentUser");
     var doucmentContent=$("#doucmentContent");
-    var documentRemark=$("#documentRemark");*/
+    var documentRemark=$("#documentRemark");
 
 
-     /*$.ajax({
+     $.ajax({
                 url: "http://localhost:8080/gwspxt/documentBaseInfo",
                 type: "get",
                 timeout: 3000,
@@ -61,13 +56,13 @@ $(function() {
                   $('#documentType').find('option[value='+data.document.documentType+']').attr('selected','selected');
                   $('#documentDept').find('option[value='+data.document.documentDept+']').attr('selected','selected');
                   $('#documentProcess').find('option[value='+data.document.documentProcess+']').attr('selected','selected');
-                *//*for(var i=1;i<data.length;i++)
+             /*  for(var i=1;i<data.length;i++)
                         {
                          $('#documentType').append("<option value='' >"+data[i].documentType.documenttypeName+"</option>");
-                        }
-*//*
+                        }*/
+
                 }
-            });*/
+            });
 
-
+})
 

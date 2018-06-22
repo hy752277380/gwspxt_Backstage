fe59eec7f880568addd0cd5b3adfe77f964afaa4
elias06 <32606960+elias06@users.noreply.github.com>
2018-06-22 10:48:32 +0800

    文档管理

--- src/main/resources/static/js/viewsJS/addDocument.js
+++ src/main/resources/static/js/viewsJS/addDocument.js
@@ -88,7 +88,7 @@ $(function () {
                                     documentId: documentId,
                                 },
                                 success: function(data) {
-                                    window.location.href = "/gwspxt/reviewDocument";
+                                    window.location.href = "/gwspxt/reviewDetailDocument";
                                 }
                             });
 
@@ -137,7 +137,7 @@ $(function () {
             data: JSON.stringify(data),
             success: function(data) {
                 if(data) {
-                    window.location.href = "/gwspxt/reviewDocument";
+                   /* window.location.href = "/gwspxt/reviewDetailDocument";*/
                     }
                     else {
 

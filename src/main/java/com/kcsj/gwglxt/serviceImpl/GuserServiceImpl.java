51b2134aa3e17271490953aacf3737cb8a916658
xiajichen <37685063+xiajichen@users.noreply.github.com>
2018-06-22 17:46:47 +0800

    修改批量删除方法

--- src/main/java/com/kcsj/gwglxt/serviceImpl/GuserServiceImpl.java
+++ src/main/java/com/kcsj/gwglxt/serviceImpl/GuserServiceImpl.java
@@ -193,15 +193,19 @@ public class GuserServiceImpl implements GuserService {
     @Override
     public int batchDelete(String[] userIds) {
         int result = 0;
-        //遍历id删除
-        for (String userId:userIds){
-            Guser guser = new Guser();
-            guser.setUserId(userId);
-            guser.setUserIsdelete(1);
-            int deleteResult = guserMapper.updateByPrimaryKeySelective(guser);
-            result = result + deleteResult;
+        if (userIds==null&&"".equals(userIds)){
+            return 0;
+        }else{
+            //遍历id删除
+            for (String userId:userIds){
+                Guser guser = new Guser();
+                guser.setUserId(userId);
+                guser.setUserIsdelete(1);
+                int deleteResult = guserMapper.updateByPrimaryKeySelective(guser);
+                result = result + deleteResult;
+            }
+            return result;
         }
-        return result;
     }
 
     @Override

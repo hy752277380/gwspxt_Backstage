920fa9c7e56aa8f12e6a0151619dbe3a5f084d9d
elias06 <32606960+elias06@users.noreply.github.com>
2018-06-22 11:13:18 +0800

    文档管理

--- src/main/java/com/kcsj/gwglxt/controller/DocumentManage/DocumentManageController.java
+++ src/main/java/com/kcsj/gwglxt/controller/DocumentManage/DocumentManageController.java
@@ -211,8 +211,6 @@ public class DocumentManageController {
     @RequestMapping("/getDocumentByState")
 
     public QueryForPage getDocumentByState(String documentType, Integer documentConfidential, Integer documentState, int currentPage, String searchInfo, HttpSession httpSession) {
-
-
         //获取session内容
         LoginCustom loginCustom = (LoginCustom) httpSession.getAttribute("LoginInformation");
         QueryForPage queryForPage = documentService.getDocumentByState(documentType, documentConfidential, documentState, loginCustom.getGuser().getUserId(), currentPage, searchInfo);

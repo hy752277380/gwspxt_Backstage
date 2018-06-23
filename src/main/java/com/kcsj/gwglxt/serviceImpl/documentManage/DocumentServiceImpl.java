1a9250825ec9a52aea060e3bc93479ba06cdee79
xiajichen <37685063+xiajichen@users.noreply.github.com>
2018-06-23 20:53:12 +0800

    改查询日志方法

--- src/main/java/com/kcsj/gwglxt/serviceImpl/documentManage/DocumentServiceImpl.java
+++ src/main/java/com/kcsj/gwglxt/serviceImpl/documentManage/DocumentServiceImpl.java
@@ -528,8 +528,14 @@ public class DocumentServiceImpl implements DocumentService {
     }
 
     @Override
-    public List<Log> getLog(int year, String userId) {
-        return logMapper.getLogByUser(year,userId);
+    public List<Object> getLog(int year, String userId) {
+        List<Object> list = new ArrayList<>();
+        //从一月开始循环查询
+        for(int mouth=1;mouth<=12;mouth++){
+            List<Log> logs = logMapper.getLogByUser(year,userId,mouth);
+            list.add(logs);
+        }
+        return list;
     }
 
     @Override
